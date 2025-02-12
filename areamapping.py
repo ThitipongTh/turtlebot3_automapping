f280e2735bc7dfccfcbd5a202304fe0d69ea3e4a
Thitipong T <james.ttpts@gmail.com>
2025-02-12 12:14:45 +0700

    update map sizing

--- areamapping.py
+++ areamapping.py
@@ -2,19 +2,19 @@ import cv2
 import numpy as np
 import yaml
 
-map_image_path = "/home/thitipongth/catkin_ws/src/map.pgm"
-yaml_path = "/home/thitipongth/catkin_ws/src/map.yaml"
+map_image_path = "/home/thitipongth/map.pgm"
+yaml_path = "/home/thitipongth/map.yaml"
 
 map_image = cv2.imread(map_image_path, cv2.IMREAD_GRAYSCALE)
 
 with open(yaml_path, 'r') as file:
     map_metadata = yaml.safe_load(file)
-resolution = map_metadata['resolution']
+resolution = map_metadata['resolution'] * 2.75
 
 _, thresholded = cv2.threshold(map_image, 200, 255, cv2.THRESH_BINARY_INV)
 
 occupied_pixels = np.sum(thresholded == 255)
 
 # Calculate the mapped area in m²
-mapped_area = occupied_pixels * (resolution ** 2) *10.0
+mapped_area = occupied_pixels * (resolution ** 2)
 print(f"Mapped Area: {mapped_area:.2f} m²")
